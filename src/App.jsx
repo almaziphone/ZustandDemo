02a2ab5036ffe7ee77202962eb7b9c2a2629f633
almaziphone <almaziphone@gmail.com>
2025-11-10 17:45:14 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Details from './components/Details'
+import Scene from './components/Scene'
+
+export default function App() {
+  return (
+    <>
+      <Scene />
+      <Details />
+    </>
+  )
+}
